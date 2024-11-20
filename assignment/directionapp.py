670501e4c54e482a3fa56178030c2fd48509c44e
SantaDave0 <davidisiani2@gmail.com>
2024-11-20 14:51:32 +0100

    direction.py

--- assignment/directionapp.py
+++ assignment/directionapp.py
@@ -2,3 +2,23 @@
 #print 
 #input
 #if law do this, elif nwabueze, elif infra , else we dont have your address
+
+print("Welcome to Godfrey Okoye")
+
+#A = input("Are you coming from law faculty?")
+#B = input("Are you coming from Nwabueze Hostel?")
+#C = input("Are you coming from Infra")
+
+location = input("Where are you coming from: ")
+
+if(location == "nwabueze"):
+    print("Keep going front, then turn left and enter the gate. Go left again and keep going straight, then turn right. Then you see a building ahead, turn right and then left. The building you just arrived is the school's clinic")
+
+elif(location == "Law faculty"):
+    print("Go straight and then turn left, you'd pass a place called crispy. Keep going straight then turn right, you would see a gate ahead. When you've entered the gate keep going straight till you pass a place called 21 step and then turn left. Then you see a building ahead, turn right and then left. The building you just arrived is the school's clinic")
+
+elif(location == "Infra"):
+    print("You finna meet King_Nasir he gonna teach you some moves innit. When at infra you beat the hell out of your meat up and the you will land at front gate. Bro just oil the fuck outta your life. Go left again and keep going straight, then turn right. Then you see a building ahead, turn right and then left. The building you just arrived is the school's clinic")
+
+else:
+    print("You're a wayward man. You have missed your direction.")
